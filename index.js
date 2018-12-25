55379bc5dfcd222a251d66757e1720b9698e1824
Brian Mugweru <brian@ralphowino.com>
2018-12-25 03:31:35 +0300

    updates

--- index.js
+++ index.js
@@ -2,30 +2,34 @@ const fs = require('fs');
 module.exports = {
   logOnRequest() {
     return (req, res, next) => {
-      let logger = {
-        "pid": process.pid,
-        "hostname": req.hostname,
-        "msg": req.body,
-        "query": req.query,
-        "time": new Date(),
-        "req": {
-          "id": Date.now(),
-          "method": req.method,
-          "url": req.url,
-          "headers": {
-            "host": req.headers.host,
-            "connection": req.headers.connection
-          },
-          "remoteAddress": req.connection.remoteAddress,
-          "remotePort": req.connection.remotePort,
-        }
-      }
-      fs.appendFile(__dirname+'/logging.txt', JSON.stringify(logger)+'\n', ((err) => {
+      console.log(formatData(req, res));
+      fs.appendFile(__dirname+'/logging.txt', JSON.stringify(formatData(req, res))+'\n', ((err) => {
         if(err) throw err;
         process.stdout.write('logs saved in logging.txt\n');
       }));
       process.stdout.write(JSON.stringify(logger)+'\n');
       next();
+    }
+  }
+}
+
+formatData = (req, res) => {
+  return {
+    "pid": process.pid,
+    "hostname": req.hostname,
+    "msg": req.body,
+    "query": req.query,
+    "time": new Date(),
+    "req": {
+      "id": Date.now(),
+      "method": req.method,
+      "url": req.url,
+      "headers": {
+        "host": req.headers.host,
+        "connection": req.headers.connection
+      },
+      "remoteAddress": req.connection.remoteAddress,
+      "remotePort": req.connection.remotePort,
     }
   }
 }
--- test/logger_test.js
+++ test/logger_test.js
@@ -1,1 +1,14 @@
-const logger = require('../index');
+const fs = require('fs');
+const request = require('supertest');
+const app = require('../index');
+const http = require('http');
+const { logOnRequest } = require('..');
+
+describe('logOnRequest', () => {
+  it('returns current timeStamp as id', (done) => {
+  });
+
+  it('formats data', (done) => {
+  });
+});
+
